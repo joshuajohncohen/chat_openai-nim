feaea138cf1469bd30ff024a4c05c4084e53d513
Joshua Cohen <65085812+joshuajohncohen@users.noreply.github.com>
2023-05-22 08:49:37 -0400

    Update chat_openai.nim and hopefully fix bug
    Signed-off-by: Joshua Cohen <65085812+joshuajohncohen@users.noreply.github.com>

--- src/chat_openai.nim
+++ src/chat_openai.nim
@@ -155,14 +155,25 @@ proc main() =
           styledEcho(styleBright, fgMagenta, "\nProgram quit with ^C\n")
           quit()
         of '\x7f':
-          if userInput.len > 0 and userInput[^1] != '\n':
-            userInput = userInput[0 ..< ^1]
-            cursorBackward(1)
-            stdout.write(" ")
-            cursorBackward(1)
-          if userInput[^1] == '\n':
-            userInput = userInput[0 ..< ^1]
-            cursorUp(1)
+          if userInput.len > 0:
+            if userInput[^1] == '\n':
+              userInput = userInput[0 ..< ^1]
+              cursorUp(1)
+              setCursorXPos((userInput.len + 5) mod terminalWidth())
+              stdout.write(" ")
+              cursorBackward(1)
+            elif (userInput.len + 5) mod terminalWidth() == 0:
+              userInput = userInput[0 ..< ^1]
+              cursorUp(1)
+              setCursorXPos(terminalWidth() - 1)
+              stdout.write(" ")
+              cursorUp(1)
+              setCursorXPos(terminalWidth() - 1)
+            else:
+              userInput = userInput[0 ..< ^1]
+              cursorBackward(1)
+              stdout.write(" ")
+              cursorBackward(1)
         of '\r':
           if userInput[^1] == '\\':
             userInput &= "\n"
